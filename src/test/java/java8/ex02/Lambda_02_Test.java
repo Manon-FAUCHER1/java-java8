972d352efcb9bfea83e386f57670e6585fbb0295
manon <manon.faucher.pro@gmail.com>
2021-03-25 12:28:31 +0100

    commit lamda exercices fini

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -8,36 +8,36 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
  * Exercice 02 - Map
  */
 public class Lambda_02_Test {
 
-    // tag::PersonToAccountMapper[]
-    interface PersonToAccountMapper {
-        Account map(Person p);
-    }
-    // end::PersonToAccountMapper[]
+	// tag::PersonToAccountMapper[]
+	interface PersonToAccountMapper {
+		Account map(Person p);
+	}
+	// end::PersonToAccountMapper[]
 
-    // tag::map[]
-    private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
-        List<Account> accounts = new ArrayList<>();
-        personList.forEach(p -> accounts.add(mapper.map(p)));
-        return accounts;
-    }
-    // end::map[]
+	// tag::map[]
+	private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
+		List<Account> accounts = new ArrayList<>();
+		personList.forEach(p -> accounts.add(mapper.map(p)));
+		return accounts;
+	}
+	// end::map[]
 
-
-    // tag::test_map_person_to_account[]
-    @Test
+	// tag::test_map_person_to_account[]
+	@Test
     public void test_map_person_to_account() throws Exception {
 
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO transformer la liste de personnes en liste de comptes
         // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+        
+        
+        List<Account> result = map(personList, p -> new Account(p, 100));
 
         assert result.size() == personList.size();
         for (Account account : result) {
@@ -45,5 +45,5 @@ public class Lambda_02_Test {
             assert account.getOwner() != null;
         }
     }
-    // end::test_map_person_to_account[]
+	// end::test_map_person_to_account[]
 }
